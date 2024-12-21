b84c23317e3ded8b478af5ed613656828cb0256d
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2024-12-21 23:56:01 +0900

    style: チェックボックスコンポーネントのスタイルを改善し、サブタイトルをboldする

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import CheckBoxs from '@/components/CheckBoxs';
+
 const Page: React.FC = () => {
   return (
     <div className="container mx-auto p-4 flex flex-col gap-4 items-center justify-cente">
-      <h1>Home</h1>
+      <CheckBoxs />
     </div>
   );
 };
--- src/components/CheckBoxs.tsx
+++ src/components/CheckBoxs.tsx
@@ -4,23 +4,42 @@ const CheckBoxs: React.FC = () => {
   return (
     <div>
       <div>
-        <p className="text-2xl font-bold text-center">都道府県</p>
+        <p className="text-2xl font-bold text-center text-pink-600">都道府県</p>
       </div>
-      <div className="mt-4 grid grid-cols-3 md:grid-cols-10 gap-2">
+      <div className="mt-4 grid grid-cols-3 md:grid-cols-10 gap-2 text-xl text-pink-500">
         {[
           { id: 'tokyo', name: '東京ダミー' },
           { id: 'osaka', name: '大阪ダミー' },
           { id: 'kyoto', name: '京都ダミー' },
           { id: 'aichi', name: '愛知ダミー' },
         ].map((prefecture) => (
-          <label key={prefecture.id} htmlFor={`pref-${prefecture.id}`}>
+          <label
+            key={prefecture.id}
+            htmlFor={`pref-${prefecture.id}`}
+            className="flex items-center cursor-pointer select-none"
+          >
             <input
               type="checkbox"
               id={`pref-${prefecture.id}`}
               name={prefecture.name}
               value={prefecture.id}
+              className="peer sr-only"
             />
-            {prefecture.name}
+            <span
+              className="
+                inline-block h-5 w-5 rounded-md border border-pink-300 bg-pink-100
+                relative transition-colors duration-200 ease-in-out
+                peer-hover:bg-pink-200 peer-hover:border-pink-400
+                peer-checked:bg-pink-200 peer-checked:border-pink-400
+                peer-checked:before:content-['✔'] peer-checked:before:text-blue-400
+                peer-checked:before:absolute peer-checked:before:left-[0.20rem]
+                peer-checked:before:top-[-0.05rem] peer-checked:before:text-sm
+              "
+            ></span>
+
+            <span className="ml-2 text-pink-600 hover:text-blue-400">
+              {prefecture.name}
+            </span>
           </label>
         ))}
       </div>
