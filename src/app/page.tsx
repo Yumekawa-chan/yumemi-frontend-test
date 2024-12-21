a33ce23ffd3a1cfb729017d9c2fd10a16319e949
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2024-12-21 23:36:44 +0900

    style: メインコンポーネントのdivにクラスを与えた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 const Page: React.FC = () => {
   return (
-    <div>
+    <div className="container mx-auto p-4 flex flex-col gap-4 items-center justify-cente">
       <h1>Home</h1>
     </div>
   );
