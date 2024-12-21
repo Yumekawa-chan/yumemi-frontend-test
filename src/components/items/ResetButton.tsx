61e1204d31b9456bbfd7bf8be060504ed1db741d
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2024-12-22 01:35:19 +0900

    add: リセットボタンを追加
    resetbuttonからimport reactを削除

---
+++ src/components/items/ResetButton.tsx
@@ -0,0 +1,9 @@
+const ResetButton: React.FC = () => {
+  return (
+    <button className="px-4 py-2 bg-blue-400 text-white rounded hover:bg-blue-500">
+      選択解除
+    </button>
+  );
+};
+
+export default ResetButton;
