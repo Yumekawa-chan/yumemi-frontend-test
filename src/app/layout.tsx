62f039b76a8a1ec89e47c6f9616c4d67834f1621
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2024-12-21 23:08:06 +0900

    update: fontをドットにした

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,32 @@
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
+import { JSX } from 'react';
+import localFont from 'next/font/local';
+
+export const metadata: Metadata = {
+  title: 'Yumemi Frontend Coding Test',
+};
+
+const dotGothic16 = localFont({
+  src: [
+    {
+      path: '../../public/DotGothic16-Regular.ttf',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-dotgothic16',
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>): JSX.Element {
   return (
     <html lang="ja">
-      <body>
+      <body
+        className={`${dotGothic16.className}`}
+      >
         {children}
       </body>
     </html>
