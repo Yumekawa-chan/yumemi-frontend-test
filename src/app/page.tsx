0f84a1c90ad0c21a1f8898c0caba5b12faed2bca
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2024-12-22 13:55:04 +0900

    add: エラー時にErrorMessageコンポーネントを返すようにした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,21 @@
 import CheckBoxs from '@/components/CheckBoxs';
 import GraphDisplay from '@/components/GraphDisplay';
 import ItemSection from '@/components/ItemSection';
+import ErrorMessage from '@/components/items/ErrorMessage';
 import LoadingSpinner from '@/components/items/LoadingSpinner';
 import { usePrefectures } from '@/hooks/usePrefectures';
 import { useState } from 'react';
 
 const Page: React.FC = () => {
   const [selectedPrefectures, setSelectedPrefectures] = useState<string[]>([]);
-  const { loading: prefecturesLoading } = usePrefectures();
+  const { loading: prefecturesLoading, error: prefectureError } =
+    usePrefectures();
   const loading = prefecturesLoading;
+  const error = prefectureError;
   if (loading) {
     return <LoadingSpinner />;
+  } else if (error) {
+    return <ErrorMessage message={error} />;
   }
   return (
     <div className="container mx-auto p-4 flex flex-col gap-4 items-center justify-center">
