8d6a174e67033cf6bd39eb0c2ae4d72c0505f0ce
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2024-12-24 01:41:20 +0900

    chore: jestでテストを行うための設定をした
    configファイルをts用にした
    
    chore: configにエイリアスを設定した
    
    add: ダミーのapi-keyを追加した

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)', '**/?(*.)+(test).[jt]s?(x)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
---
+++ jest.setup.ts
@@ -0,0 +1,5 @@
+import '@testing-library/jest-dom';
+
+process.env.NEXT_PUBLIC_PREFECTURES_API_URL = 'http://test/prefectures';
+process.env.NEXT_PUBLIC_POPULATION_API_URL = 'http://test/population';
+process.env.NEXT_PUBLIC_API_KEY = 'TEST_KEY';
