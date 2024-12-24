451313c92a19cab150ed2875e5bfba5ba5a6afb9
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2024-12-24 12:53:35 +0900

    chore: tsxのテストのための設定
    remove: js,jsxは今回必要ないので削除

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,15 @@
 import '@testing-library/jest-dom';
 
+if (typeof window !== 'undefined') {
+  if (!window.ResizeObserver) {
+    window.ResizeObserver = class ResizeObserver {
+      disconnect() {}
+      observe() {}
+      unobserve() {}
+    };
+  }
+}
+
 process.env.NEXT_PUBLIC_PREFECTURES_API_URL = 'http://test/prefectures';
 process.env.NEXT_PUBLIC_POPULATION_API_URL = 'http://test/population';
 process.env.NEXT_PUBLIC_API_KEY = 'TEST_KEY';
